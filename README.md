f8c97190451b969c6894c848cd3847177eea25a3
mihaidld <mihaidoldur@gmail.com>
2020-12-27 07:38:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# To-Do App - localStorage
+
+## Features
+
+- project set up using `Create React App` and using `React Hooks API` (_useState_, _useEffect_, _useContext_)
+- list saved in `localStorage`
+- styled components with `Bootstrap 5`
+- modular components
+- responsive, accessible and with a dark mode preference saved in `localStorage`
+
+## Install dependencies
+
+```zsh
+% yarn install
+```
+
+## Run the app in the development mode
+
+```zsh
+% yarn start
+```
---
+++ src/components/Todos.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import TodosList from "./TodosList";
+import SelectTodos from "./SelectTodos";
+import AddTodoForm from "./AddTodoForm";
+import { v4 as uuidv4 } from "uuid";
+
+const initialTodos = [
+  {
+    text: "Faires des courses",
+    isCompleted: true,
+    id: "1b688c51-e990-4ce3-95a5-9018cf81d23d",
+  },
+  {
+    text: "Réviser ES6 classes",
+    isCompleted: false,
+    id: "efc6331d-7ca2-49a6-b014-378b8280b33d",
+  },
+  {
+    text: "Aroser les plantes",
+    isCompleted: false,
+    id: "9e60d353-cd72-40bb-97e6-5841e51635c0",
+  },
+];
+
+const Todos = () => {
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem("todo-list")) || initialTodos
+  );
+  const [filter, setFilter] = useState("all");
+
+  const addTodo = (text) => {
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (task) => {
+    setTodos(todos.filter((el) => el.id !== task.id));
+  };
+
+  const toggleCompleteTodo = (task) => {
+    setTodos(
+      todos.map((el) => {
+        if (el.id === task.id) {
+          el.isCompleted = !el.isCompleted;
+        }
+        return el;
+      })
+    );
+  };
+
+  useEffect(() => {
+    document.title = todos.length
+      ? `Vous avez ${todos.length} tâches à accomplir !`
+      : "Que devez vous faire aujourd'hui ?";
+  }, [todos.length]);
+
+  useEffect(() => {
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+  }, [todos]);
+
+  const filteredTodos = todos.filter((el) => {
+    if (filter === "completed") {
+      return el.isCompleted;
+    }
+    if (filter === "notcompleted") {
+      return !el.isCompleted;
+    }
+    return true;
+  });
+
+  const completedCount = todos.filter((el) => el.isCompleted).length;
+  return (
+    <main>
+      <h2 className="mb-3">
+        Ma liste de tâches ({completedCount} / {todos.length})
+      </h2>
+      <SelectTodos filter={filter} setFilter={setFilter} />
+      <TodosList
+        todos={filteredTodos}
+        deleteTodo={deleteTodo}
+        toggleCompleteTodo={toggleCompleteTodo}
+      />
+      <AddTodoForm addTodo={addTodo} setFilter={setFilter} />
+    </main>
+  );
+};
+
+export default Todos;
